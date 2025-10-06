17f90e88b552eb475ea0d5a2ea5fca9ab608be49
ewar4523 <139654177+ewar4523@users.noreply.github.com>
2025-10-06 16:24:57 +0000

    Сделал 1-5

---
+++ main.py
@@ -0,0 +1,37 @@
+print("hello, World!\nМаша + Петя = Любовь\nx=3+4\nx=7")
+
+num = [1, 434,5]
+print(type("hello, world"))
+print(type(3+4))
+print(type(3/4))
+print(type(num))
+
+x=3
+y1 = (((x**3)**(1/2)) / (x**3+(3/x))) * (4*x**7 - x**5) + 80 * ((27 * x**4 + 12 * x**3 - 5 * x**2 + 10) ** (1/2))
+y2 = (3/2 + round(16.7 * 4.32)) / (14.5 + (31/12) - round(x**3.4))
+print(y1, y2)
+
+list1 = [1, 5, 'Good', 'Bad']
+list2 = [9, 'Blue', 'Red', 11]
+print(list1[1]+list2[3])
+print(list1[2]+list2[2])
+print(list1[0]*list2[0])
+print(list1[1]**list2[3])
+print(list1+list2)
+
+print("Шляпка гриба, покрытая... кожиц..й, держится на ... ножк.. . Снизу шляпка затя... плёнкой. Когда её уберешь, откроется нижняя ... сторона шляпк.. ." )
+print("покрытая ...")
+fail1 = input()
+print("кожиц..й")
+fail2 = input()
+print("держится на ...")
+fail3 = input()
+print("ножк..")
+fail4 = input()
+print("шляпка затя...")
+fail5 = input()
+print("откроется нижняя ...")
+fail6 = input()
+print("сторона шляпк..")
+fail7 = input()
+print(f"Шляпка гриба, покрытая {fail1} кожиц{fail2}й, держится на {fail3} ножк{fail4}. Снизу шляпка затя{fail5} плёнкой. Когда её уберешь, откроется нижняя {fail6} сторона шляпк{fail7}.")
